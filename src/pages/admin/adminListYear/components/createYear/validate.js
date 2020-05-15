c20e8adcc3ed80db9488c907962901a460af5f05
iamz-tao <srikenkan@gmail.com>
2020-05-15 10:04:32 +0700

    Validate create year

--- src/pages/admin/adminListYear/components/createYear/validate.js
+++ src/pages/admin/adminListYear/components/createYear/validate.js
@@ -3,11 +3,17 @@ import set from 'lodash/set'
 const validate = (values) => {
   const errors = {}
 
-  if (values.get('year') === undefined ) {
+  if (values.get('year') === undefined) {
     set(errors, 'year', 'Required')
   }
+
+  // eslint-disable-next-line no-restricted-globals
   if (isNaN(values.get('year'))) {
     errors.year = 'Invalid year!'
+  }
+  // eslint-disable-next-line no-bitwise
+  if (~~values.get('year') > new Date().getFullYear() - 543) {
+    errors.year = 'No more than the current year.'
   }
 
   if (values.get('semester') === undefined) {
